f8ab38668855241e20d5eac1cc58641e8e264e8d
CEINMARK <CEINMARK@DESKTOP-4GR91FV>
2021-02-24 12:07:42 +0100

    hola mario

---
+++ src/prueba/PruebaMario.java
@@ -0,0 +1,25 @@
+package prueba;
+
+import java.util.Scanner;
+
+public class PruebaMario {
+	
+	public static void main(String arg []) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num,num1;
+		
+		num=sc.nextInt();
+		num1=sc.nextInt();
+		
+		
+		
+	}
+	
+
+	
+	
+	
+}
